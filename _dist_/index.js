9d19f281e883f36de981d3e7408cf3d9244e5232
arielgomez-py <arielgomez-py@users.noreply.github.com>
2022-06-08 16:21:01 +0000

    deploy: efa8e35421c07fbcfcd9f8116cb210707660dd01

--- _dist_/index.js
+++ _dist_/index.js
@@ -5,7 +5,27 @@ const getInfo = async() => {
         const response = await fetch(urlBase);
         const data = await response.json();
         console.log(data.results);
+
+        const input = document.getElementById("name");
+        const submitButton = document.getElementById("submit-btn");
+        
+        //Validacion
+        const validacion = () => {
+            
+            if(data.results[i].name === input.value){
+                alert("Ahiiiii");
+            }
+            else{
+                alert("Error");
+            }
+            
+        }
+
+        //Events
+        submitButton.addEventListener("click", validacion);
+        
     }
+
     catch(error){
         console.log(error);
     }
@@ -14,7 +34,7 @@ const getInfo = async() => {
     const container = document.getElementById("main-container");
 
     // Create nodes
-    const addNodes = () => {
+    /*const addNodes = () => {
         const card = document.createElement('article');
         const image = document.createElement('img');
         const paragraph = document.createElement('p');
@@ -22,9 +42,11 @@ const getInfo = async() => {
         container.append(card);
         card.append(image, paragraph);
     }
-    
+    */
+
+
     const submitButton = document.getElementById("submit-btn");
-    submitButton.addEventListener("submit", addNodes);
+    //submitButton.addEventListener("submit", validacion);
     
 }
 
